1371b209c4fc11e165a4467a58e36d0942de11b1
Rishi Pomal <38519496+rishi07it417@users.noreply.github.com>
2018-04-19 18:55:15 +0530

    Sample Hibernate Project

--- TestHibernateProject/src/com/test/dao/EmployeeDao.java
+++ TestHibernateProject/src/com/test/dao/EmployeeDao.java
@@ -1,9 +1,17 @@
 package com.test.dao;
 
+import java.util.List;
+
+import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.AnnotationConfiguration;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.criterion.Criterion;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Projection;
+import org.hibernate.criterion.Projections;
+import org.hibernate.criterion.Restrictions;
 
 import com.test.model.Employee;
 
@@ -15,22 +23,85 @@ public class EmployeeDao {
 		try{
 			SessionFactory sessionFactory = new AnnotationConfiguration().configure(EmployeeDao.class.getClassLoader().getResource("hibernate.cfg.xml")).buildSessionFactory();	
 			Session se = sessionFactory.openSession();
-			System.out.println(" ------ before new Employee ");
+			 
+			boolean isInserted = saveEmp(se,"Rishi1","Developer",25000);
+			System.out.println("*********************Insertion Result"+ isInserted);
+			
+			boolean isUpdate = updateEmp(se,"Rishi1","Support",15000);
+			System.out.println("*********************Updation Result"+ isUpdate);
 
-			Employee emp = new Employee();
-			System.out.println(" ------ after new Employee ");
-			se.beginTransaction();
-			//emp.setId(2);
-			emp.setName("Rishi");
-			emp.setDepartment("IT");
-			emp.setSalary(15000);
-			System.out.println(" ------ before save ");
-			se.save(emp);
-			System.out.println(" ------ after save ");
-se.getTransaction().commit();
+			List<Employee> emp = getEmp(se);
+			System.out.println("*********************Employee Get Result"+ emp.toString());
+
+			
+			se.getTransaction().commit();
 			se.close();
 		}catch(Exception e){
 			e.printStackTrace();
 		}
 	}
+
+
+public static boolean saveEmp(Session se,String name,String department,Integer salary){
+	try{
+		System.out.println(" ------ Inside Save EMP----------- ");
+
+		Employee emp = new Employee();
+		se.beginTransaction();
+		emp.setName(name);
+		emp.setDepartment(department);
+		emp.setSalary(salary);
+		System.out.println(" ------ before save ");
+		se.save(emp);
+		System.out.println(" ------ after save ");
+	}catch (Exception e) {
+		e.printStackTrace();
+		return false;
+	}
+	return true;
 }
+
+
+public static boolean updateEmp(Session se,String name,String department,Integer salary){
+	try{
+		System.out.println(" ------ Inside Save EMP----------- ");
+
+		Employee emp = new Employee();
+		se.beginTransaction();
+		emp.setName(name);
+		emp.setDepartment(department);
+		emp.setSalary(salary);
+		System.out.println(" ------ before save ");
+		se.saveOrUpdate(emp);
+		System.out.println(" ------ after save ");
+	}catch (Exception e) {
+		e.printStackTrace();
+		return false;
+	}
+	return true;
+}
+
+public static List<Employee> getEmp(Session se){
+	List<Employee> empList = null;
+	try{
+		System.out.println(" ------ Inside Save EMP----------- ");
+		
+		se.beginTransaction();
+		
+		Criteria cr = se.createCriteria(Employee.class);
+		Criterion c1 = Restrictions.ge("salary", 20000);
+	
+		cr.add(c1);
+		cr.setFetchSize(10);
+		cr.addOrder(Order.desc("salary"));
+		 
+		 empList = cr.list();
+		
+	}catch (Exception e) {
+		e.printStackTrace();
+	}
+	return empList;
+}
+
+}
+
